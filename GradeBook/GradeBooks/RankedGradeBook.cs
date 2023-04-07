1cf07ec9880b90c76ccb7df5b27b324f0b241650
pgr982 <patrykgromotka98@gmail.com>
2023-04-07 21:49:57 +0200

    Test 24/24

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -7,7 +7,7 @@ using System.Xml.Linq;
 
 namespace GradeBook.GradeBooks
 {
-    internal class RankedGradeBook : BaseGradeBook
+    public class RankedGradeBook : BaseGradeBook
     {
         public RankedGradeBook(string name,bool isWeighted) : base(name, isWeighted)
         {
