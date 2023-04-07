68658366edcf0aee85ed1c1e9b76db2eba470acd
pgr982 <patrykgromotka98@gmail.com>
2023-04-07 20:10:26 +0200

    Override  RankedGradeBook 's  CalculateStatistics  method, Override  RankedGradeBook 's  CalculateStudentStatistics  method, test 11/24

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -42,5 +42,31 @@ namespace GradeBook.GradeBooks
             return 'F';
 
         }
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students");
+                return;
+            }
+            else
+            {
+                base.CalculateStatistics();
+            }
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students");
+                return;
+            }
+            else
+            {
+                base.CalculateStudentStatistics(name);
+            }
+        }
+
     }
 }
