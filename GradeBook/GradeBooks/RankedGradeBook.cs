6acaff9c19008d9aae4f9fc661194566a4424827
pgr982 <patrykgromotka98@gmail.com>
2023-04-07 18:50:42 +0200

    RankedGradeBook 's  GetLetterGrade  method, test 9/24

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeBook.GradeBooks
+{
+    internal class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = Enums.GradeBookType.Ranked;
+        }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("You need at least 5 students to do ranked grading");
+            }
+
+            var n = (int)Math.Ceiling(Students.Count * 0.2);
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            if (averageGrade >= grades[n - 1])
+            {
+                return 'A';
+            }
+            if (averageGrade >= grades[(n * 2) - 1])
+            {
+                return 'B';
+            }
+            if (averageGrade >= grades[(n * 3) - 1])
+            {
+                return 'C';
+            }
+            if (averageGrade >= grades[(n * 4) - 1])
+            {
+                return 'D';
+            }
+
+            return 'F';
+
+        }
+    }
+}
